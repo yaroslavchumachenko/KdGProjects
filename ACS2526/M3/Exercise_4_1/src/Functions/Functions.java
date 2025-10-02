f17ac2786cc75839dd2d2bac9a804a14096a88ed
yaroslavchumachenko <yaroslav.chumachenko.off@gmail.com>
2025-10-02 15:54:48 +0200

    Refresh

--- ACS2526/M3/Exercise_4_1/src/Functions/Functions.java
+++ ACS2526/M3/Exercise_4_1/src/Functions/Functions.java
@@ -3,6 +3,6 @@ package Functions;
 public class Functions extends Scrabble{
     public static void main(String[] args) {
         printTotal(MyCount(MyInp()));
-        System.out.println("check");
+
     }
 }
